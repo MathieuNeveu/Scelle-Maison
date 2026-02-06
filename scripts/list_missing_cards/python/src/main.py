1ac029121fc28b039ba81842b174ccd200d67459
MathieuNeveu <m@mathn.fr>
2026-02-06 15:48:43 +0100

    Hello world unittest

---
+++ scripts/list_missing_cards/python/src/main.py
@@ -0,0 +1,5 @@
+def ma_fonction():
+    return "Hello World que j'aime"
+
+if __name__ == '__main__':
+    ma_fonction()
---
+++ scripts/list_missing_cards/python/tests/test_main.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.main import ma_fonction
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_outputlength(self):
+        self.assertEqual(len(ma_fonction()), len('Hello World'))
+
+    def test_isHelloworld(self):
+        self.assertEqual(ma_fonction(), 'Hello World')
+
+if __name__ == '__main__':
+    unittest.main()
