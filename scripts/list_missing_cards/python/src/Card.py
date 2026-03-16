26ef787ab68662c963ae0dc276c02f3883e9a360
MathieuNeveu <m@mathn.fr>
2026-03-16 09:19:04 +0100

    :test_tube: test_instantiateHero - methods pluged together

--- scripts/list_missing_cards/python/src/Card.py
+++ scripts/list_missing_cards/python/src/Card.py
@@ -16,15 +16,26 @@ class Card:
 
     @classmethod
     def from_csv(cls, csv_file: TextIOWrapper) -> list['Card']:
-        return [cls(1, 1, 'R', 'EN', 0, 'Axiom', 'Sierra & Oddball')]
+        dict_list: list[dict] = cls.csv_to_dict_list(csv_file)
+        card_list: list[Card] = []
+        for card_dict in dict_list:
+            card_list.append(cls(
+                card_dict['_id'],
+                card_dict['index'],
+                card_dict['codeType'],
+                card_dict['lang'],
+                card_dict['units'],
+                card_dict['faction'],
+                card_dict['name']
+            ))
+        return card_list
 
     @staticmethod
-    def csv_line_to_dict(csv_file: TextIOWrapper) -> dict:
-        # csv_reader = csv.reader(csv_file, delimiter=',')
+    def csv_line_to_dict(line: list[str]) -> dict:
         return {
             '_id': None,
             'index': None,
-            'codeType': None,
+            'codeType': line[1],
             'lang': None,
             'units': None,
             'faction': None,
@@ -33,4 +44,9 @@ class Card:
 
     @staticmethod
     def csv_to_dict_list(csv_file: TextIOWrapper) -> list:
-        return []
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        dict_list: list = []
+        for line in csv_reader:
+            parsed_line: dict = Card.csv_line_to_dict(line)
+            dict_list.append(parsed_line)
+        return dict_list
