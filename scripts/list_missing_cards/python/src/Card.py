2e599ac0cf6d62b6ae83cae677180eafed450565
MathieuNeveu <m@mathn.fr>
2026-04-21 18:17:25 +0200

    do this after that

--- scripts/list_missing_cards/python/src/Card.py
+++ scripts/list_missing_cards/python/src/Card.py
@@ -23,8 +23,8 @@ class Card:
 
     @classmethod
     def from_csv(cls, csv_absolute_path: str) -> list['Card']:
-        #test csv length > 1 line
         csv_file: TextIOWrapper = cls.open_csv_file(csv_absolute_path)
+        #test csv length > 1 line
         dict_list: list[dict] = cls.csv_to_dict_list(csv_file)
         card_list: list[Card] = []
         for card_dict in dict_list:
