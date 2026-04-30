b4290cca3da2f504f001522f54fb848b2876a5d0
MathieuNeveu <m@mathn.fr>
2026-04-30 11:30:15 +0200

    :truck: :white_check_mark: (SRP) move TestOpenCSVFile to test_FileReader.py

---
+++ scripts/list_missing_cards/python/tests/test_FileReader.py
@@ -0,0 +1,49 @@
+import json
+import unittest
+
+from scripts.list_missing_cards.python.src.Card import Card
+from scripts.list_missing_cards.python.src.Exceptions.ParameterException import ParameterException
+
+class TestOpenCSVFile(unittest.TestCase):
+    def setUp(self):
+        config_file = open("../config.json")
+        json_config = json.load(config_file)
+        config_file.close()
+
+        self.config_csv_location: dict = json_config['csv']['location']
+        good_filepath: str = (
+                self.config_csv_location['parent_dir_absolute_path']+'/'+
+                self.config_csv_location['filename']
+        )
+
+        self.wrong_filepath: str = (
+                self.config_csv_location['parent_dir_absolute_path']+
+                '/bad_name.csv'
+        )
+
+    def tearDown(self):
+        return super().tearDown()
+
+    def test_wrongPathType(self):
+        wrong_parameter: int = 666
+        with self.assertRaises(ParameterException) as context:
+            Card.open_csv_file(wrong_parameter)
+        exception: ParameterException = context.exception
+        self.assertEqual(400, exception.error_code)
+        self.assertEqual(str, exception.expected_type)
+        self.assertEqual(int, exception.parameter_type)
+        self.assertEqual('csv_absolute_path', exception.parameter_name)
+        self.assertEqual(
+            "In `open_csv_file` method, `csv_absolute_path` parameter Exception (400)."
+            " <class 'int'> type founded instead of <class 'str'>",
+            exception.message
+        )
+
+    def test_wrongFileName(self):
+        with self.assertRaises(FileNotFoundError) as context:
+            Card.open_csv_file(self.wrong_filepath)
+        exception: FileNotFoundError = context.exception
+        self.assertIn(
+            self.config_csv_location['parent_dir_absolute_path'],
+            str(exception)
+        )
