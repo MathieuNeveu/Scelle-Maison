d304abb75c220f72dde445faf2c067c3f135225d
MathieuNeveu <m@mathn.fr>
2026-05-01 15:35:48 +0200

    :white_check_mark: test_exception_when_passing_int_type_as_process

--- scripts/list_missing_cards/python/src/FileReader.py
+++ scripts/list_missing_cards/python/src/FileReader.py
@@ -1,9 +1,12 @@
+from collections.abc import Callable
+from io import TextIOWrapper
+
 from scripts.list_missing_cards.python.src.Exceptions.ParameterException import ParameterException
 
 class FileReader:
 
     @staticmethod
-    def open_with_process(file_absolute_path: str, process):
+    def open_with_process(file_absolute_path: str, process: Callable[[TextIOWrapper], ...]):
         if type(file_absolute_path) is not str:
             raise ParameterException(
                 400,
@@ -11,6 +14,14 @@ class FileReader:
                 type(file_absolute_path),
                 'open_with_process',
                 'file_absolute_path'
+            )
+        if type(process) is int:
+            raise ParameterException(
+                400,
+                Callable,
+                int,
+                'open_with_process',
+                'process',
             )
         with open(file_absolute_path, 'r') as file:
             return process(file)
