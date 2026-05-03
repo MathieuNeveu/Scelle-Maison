3c63437fcf24612b757c0740f059799b09be463e
MathieuNeveu <m@mathn.fr>
2026-05-03 23:05:14 +0200

    :test_tube: TestGetDataAsDict > test_header_should_be_list

--- scripts/list_missing_cards/python/tests/test_CSVReader.py
+++ scripts/list_missing_cards/python/tests/test_CSVReader.py
@@ -5,10 +5,13 @@ from scripts.list_missing_cards.python.src.CSV import CSV
 class TestGetDataAsDict(unittest.TestCase):
     def setUp(self):
         self.csv_file: CSV = CSV()
+        self.dict_data: dict = self.csv_file.get_data_as_dict()
 
     def test_return_dict(self):
         self.assertIsInstance(self.csv_file.get_data_as_dict(), dict)
 
     def test_return_an_header_key(self):
-        dict_data: dict = self.csv_file.get_data_as_dict()
-        self.assertIn('header', dict_data)
+        self.assertIn('header', self.dict_data)
+
+    def test_header_should_be_list(self):
+        self.assertIsInstance(self.dict_data['header'], list)
