03c7dcd09b1d3b8880d9c4ab7636ca5f80b6aa9d
MathieuNeveu <m@mathn.fr>
2026-05-03 23:02:27 +0200

    :test_tube: TestGetDataAsDict > test_return_an_header_key

--- scripts/list_missing_cards/python/tests/test_CSVReader.py
+++ scripts/list_missing_cards/python/tests/test_CSVReader.py
@@ -9,3 +9,6 @@ class TestGetDataAsDict(unittest.TestCase):
     def test_return_dict(self):
         self.assertIsInstance(self.csv_file.get_data_as_dict(), dict)
 
+    def test_return_an_header_key(self):
+        dict_data: dict = self.csv_file.get_data_as_dict()
+        self.assertIn('header', dict_data)
