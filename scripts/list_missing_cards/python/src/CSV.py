95ae3147abae1cc5d203740de801e071f70ec241
MathieuNeveu <m@mathn.fr>
2026-05-03 23:03:41 +0200

    :white_check_mark: TestGetDataAsDict > test_return_an_header_key

--- scripts/list_missing_cards/python/src/CSV.py
+++ scripts/list_missing_cards/python/src/CSV.py
@@ -3,4 +3,6 @@ from scripts.list_missing_cards.python.src.File import File
 class CSV(File):
 
     def get_data_as_dict(self) -> dict:
-        return {}
+        return {
+            'header': None
+        }
