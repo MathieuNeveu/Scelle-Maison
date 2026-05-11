6c4a14ea94d5c03b84d07706ea777fd957886250
MathieuNeveu <m@mathn.fr>
2026-05-11 21:06:50 +0200

    are_parameters_ok . path param :str & :int should raise exception >OK<

--- scripts/list_missing_cards/python/tests/test_File.py
+++ scripts/list_missing_cards/python/tests/test_File.py
@@ -107,9 +107,9 @@ class TestAreParametersOk(unittest.TestCase):
     def test_should_be_boolean(self):
         self.assertIsInstance(File.are_parameters_ok(True, True), bool)
 
-   # def test_path_check_int_should_raise_exception(self):
-       # with self.assertRaises(Exception):
-           # File.are_parameters_ok(6, True)
+    def test_path_check_int_should_raise_exception(self):
+        with self.assertRaises(Exception):
+            File.are_parameters_ok(6, True)
 
     def test_path_check_str_should_raise_exception(self):
         with self.assertRaises(Exception):
