ea7f0ccc2c7cfbf7e3cad16f2cb731a9e4a3e8ef
MathieuNeveu <m@mathn.fr>
2026-05-14 15:26:44 +0200

    (refacto) using are_parameters_ok() in open_with_process()

--- scripts/list_missing_cards/python/src/File.py
+++ scripts/list_missing_cards/python/src/File.py
@@ -12,24 +12,19 @@ class File(ABC):
 
     @staticmethod
     def open_with_process(file_absolute_path: str, process: Callable[[TextIOWrapper], ...]):
-        if File.is_path_ok(file_absolute_path):
-            if not callable(process):
-                raise ParameterException(
-                    400,
-                    Callable,
-                    type(process),
-                    'open_with_process',
-                    'process',
-                )
+        if File.are_parameters_ok(
+            File.is_path_ok(file_absolute_path),
+            File.is_process_ok(process),
+        ):
             with open(file_absolute_path, 'r') as file:
                 return process(file)
         raise Exception
 
     @staticmethod
-    def are_parameters_ok(is_path_ok: bool, process: bool) -> bool:
-        if type(is_path_ok) is not bool or type(process) is not bool:
+    def are_parameters_ok(is_path_ok: bool, is_process_ok: bool) -> bool:
+        if type(is_path_ok) is not bool or type(is_process_ok) is not bool:
             raise Exception
-        return is_path_ok and process
+        return is_path_ok and is_process_ok
 
     @staticmethod
     def is_path_ok(absolute_path: str) -> bool:
